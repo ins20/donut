f55dfc430e7c35612ab764ec59784facdabc99c5
ins20 <ibragimovns@yahoo.com>
2025-01-29 21:54:05 +0400

    1

--- app/api/payment.ts
+++ app/api/payment.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest) {
   if (req.method === "POST") {
     console.log(req.body);
   }

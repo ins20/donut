58917144fc52309252022a4c7d8fc3fd8a7afd84
ins20 <ibragimovns@yahoo.com>
2025-01-29 21:56:59 +0400

    1

--- app/api/payment.ts
+++ app/api/payment.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 
 export default function handler(req: NextApiRequest) {
   if (req.method === "POST") {
